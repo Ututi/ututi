cc124eb82f6bdb5a73c7d8748e5e7c3defa943ab
Domas Monkus <domas.m@gmail.com>
2009-05-29 15:26:48 +0300

    Removed id from admin user creation.
    This messes up the sequence of the id field in the users table.

--- src/ututi/config/defaults.sql
+++ src/ututi/config/defaults.sql
@@ -2,7 +2,7 @@
 
 create table users (id bigserial not null, fullname char(20), password char(32), primary key (id));
 
-insert into users (id, fullname, password) values (1, 'Adminas Adminovič', '069edb446c4ec937e862bce38ee4c458');
+insert into users (fullname, password) values ('Adminas Adminovič', '069edb446c4ec937e862bce38ee4c458');
 
 /* Storing the emails of the users. */
 create table emails (id int8 not null references users(id), email char(320), primary key (email));
