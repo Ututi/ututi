a306fd848f649764b7a06d61f5199f6c55ef82e5
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2009-05-29 12:55:59 +0300

    Remove teardown statements from database initialization script.

--- src/ututi/config/defaults.sql
+++ src/ututi/config/defaults.sql
@@ -1,5 +1,4 @@
 /* Create first user=admin and password=asdasd */
 create table users (id bigserial not null, name char(20), password char(32), PRIMARY KEY (id));
---- drop table users;
 
 INSERT INTO users (id, name, password) VALUES (1, 'admin', '069edb446c4ec937e862bce38ee4c458');
