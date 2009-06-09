30774c8c5230963cbad610ea42e43c34dcda2f88
Domas Monkus <domas.m@gmail.com>
2009-06-09 16:02:56 +0300

    Fixed some functional tests and made them pass.
    The test check for variuos registration entry mistakes.
    Added a custom formencode validator that checks if the email already exists in the database.

--- src/ututi/controllers/home.py
+++ src/ututi/controllers/home.py
@@ -4,6 +4,7 @@ import formencode
 from pylons import request, response
 from pylons.controllers.util import redirect_to
 from pylons.decorators import validate
+from pylons.i18n import _
 
 from ututi.lib.base import BaseController, render
 
@@ -11,12 +12,48 @@ from ututi.model import meta, User, Email
 
 log = logging.getLogger(__name__)
 
+class UniqueEmail(formencode.validators.FancyValidator):
+     messages = {
+         'empty': _(u"Enter a valid email."),
+         'non_unique': _(u"The email already exists."),
+         }
+
+     def _to_python(self, value, state):
+         # _to_python gets run before validate_python.  Here we
+         # strip whitespace off the password, because leading and
+         # trailing whitespace in a password is too elite.
+         return value.strip()
+
+     def validate_python(self, value, state):
+         if value == '':
+             raise formencode.Invalid(self.message("empty", state), value, state)
+         elif meta.Session.query(Email).filter_by(email=value).count() > 0:
+             raise formencode.Invalid(self.message("non_unique", state), value, state)
+
+
 class RegistrationForm(formencode.Schema):
-    allow_extra_fields = True
-    email = formencode.validators.Email(not_empty=True)
-    new_password = formencode.validators.String()
-    repeat_password = formencode.validators.String()
-    chained_validators = [formencode.validators.FieldsMatch('new_password', 'repeat_password')]
+
+    allow_extra_fields = False
+    fullname = formencode.validators.String(not_empty = True,
+                                            messages = {
+            'empty' : _(u"Please enter your name to register."),
+            })
+    email = formencode.All(formencode.validators.Email(not_empty=True),
+                           UniqueEmail(messages = {
+                'non_unique' : _(u"This email has already been used to register.")}))
+
+    psw_msg = {'empty' : _(u"Please enter your password to register."),
+               'tooShort' : _(u"The password must be at least 5 symbols long.")}
+    new_password = formencode.validators.String(min=5, not_empty=True,
+                                                messages = psw_msg)
+    repeat_password = formencode.validators.String(min=5, not_empty=True,
+                                                   messages = psw_msg)
+    chained_validators = [formencode.validators.FieldsMatch('new_password', 'repeat_password',
+                                                            messages = {
+                'invalid' : _(u"Passwords do not match."),
+                'invalidNoMatch' : _(u"Passwords do not match."),
+                'empty' : _(u"Please enter your password to register.")}
+                )]
 
 class HomeController(BaseController):
 
@@ -42,7 +79,7 @@ class HomeController(BaseController):
 
         identity = {'repoze.who.userid': email}
         headers = request.environ['repoze.who.plugins']['auth_tkt'].remember(
-            request.environ, 
+            request.environ,
             identity)
         for k, v in headers:
             response.headers.add(k, v)
