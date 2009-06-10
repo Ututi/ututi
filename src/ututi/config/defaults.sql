6a3859b1ee74306e2bb24299d17ee11db28fab4e
Domas Monkus <domas.m@gmail.com>
2009-06-10 11:45:47 +0300

    Modified the emails table schema.
    Added a boolean field to specify whether the email has been confirmed by the user.
    The default value is set to false.

--- src/ututi/config/defaults.sql
+++ src/ututi/config/defaults.sql
@@ -5,6 +5,6 @@ create table users (id bigserial not null, fullname char(100), password char(32)
 insert into users (fullname, password) values ('Adminas Adminovič', '069edb446c4ec937e862bce38ee4c458');
 
 /* Storing the emails of the users. */
-create table emails (id int8 not null references users(id), email char(320), primary key (email));
+create table emails (id int8 not null references users(id), email char(320), confirmed boolean default FALSE, primary key (email));
 
 insert into emails (id, email) values (1, 'admin@ututi.lt');
