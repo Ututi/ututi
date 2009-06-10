37a64759bebebad8b8487f25eb0ac1d0c7480e4d
Domas Monkus <domas.m@gmail.com>
2009-06-10 18:09:11 +0300

    Added a helper function to get all the urls in a string.
    Useful in tests.

--- src/ututi/lib/helpers.py
+++ src/ututi/lib/helpers.py
@@ -1,3 +1,4 @@
+import re
 """Helper functions
 
 Consists of functions to typically be used within templates, but also
@@ -5,3 +6,6 @@ available to Controllers. This module is available to templates as 'h'.
 """
 # Import helpers as desired, or define your own, ie:
 #from webhelpers.html.tags import checkbox, password
+def get_urls(text):
+    urls = re.findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",text)
+    return urls
