c6f17b908370fde9379a0111eccaebaf3713f173
Domas Monkus <domas.m@gmail.com>
2009-06-10 15:52:57 +0300

    Updated the database schema.
    Using varchars for emails and names - simple char columns are padded to their max length with spaces, which does not work for us.
    Added two email confirmation-related columns to the emails table: confirmed and confirmation_key.

--- src/ututi/config/defaults.sql
+++ src/ututi/config/defaults.sql
@@ -1,10 +1,14 @@
 /* Create first user=admin and password=asdasd */
 
-create table users (id bigserial not null, fullname char(100), password char(32), primary key (id));
+create table users (id bigserial not null, fullname varchar(100), password char(32), primary key (id));
 
 insert into users (fullname, password) values ('Adminas Adminovič', '069edb446c4ec937e862bce38ee4c458');
 
 /* Storing the emails of the users. */
-create table emails (id int8 not null references users(id), email char(320), confirmed boolean default FALSE, primary key (email));
+create table emails (id int8 not null references users(id),
+       email varchar(320),
+       confirmed boolean default FALSE,
+       confirmation_key char(32) default '',
+       primary key (email));
 
 insert into emails (id, email) values (1, 'admin@ututi.lt');
