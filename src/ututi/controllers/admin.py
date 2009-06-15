90efd1361bad6799ca0a8bd696ddee0c84744f17
Domas Monkus <domas.m@gmail.com>
2009-06-15 14:18:22 +0300

    Added a form and a view to import location (universities and faculties) from plone.

--- src/ututi/controllers/admin.py
+++ src/ututi/controllers/admin.py
@@ -4,11 +4,11 @@ from pylons import request, response, c
 from pylons.controllers.util import redirect_to
 from pylons.decorators import validate
 from pylons.i18n import _
-
+from sqlalchemy.orm.exc import NoResultFound
 from ututi.lib.base import BaseController, render
 from ututi.lib import current_user, email_confirmation_request
 
-from ututi.model import meta, User, Email
+from ututi.model import meta, User, Email, LocationTag
 
 log = logging.getLogger(__name__)
 
@@ -30,7 +30,8 @@ class AdminController(BaseController):
 
     def import_users(self):
         file = request.POST.get('file_upload', None)
-        if file is not None:
+
+        if file is not None and file != '':
             for line in file.value.split('\n'):
                 if line.strip() == '':
                     continue
@@ -38,9 +39,50 @@ class AdminController(BaseController):
                 fullname = line[2].strip()
                 password = line[1].strip()[6:]
                 email = line[3].strip()
-                user = User(fullname, password, False)
-                user.emails = [Email(email)]
+                try:
+                    user = meta.Session.query(Email).filter_by(email = email).one().user
+                except NoResultFound:
+                    user = User(fullname, password, False)
+                    user.emails = [Email(email)]
+                    meta.Session.add(user)
 
-                meta.Session.add(user)
                 meta.Session.commit()
         redirect_to(controller='admin', action='users')
+
+    def import_structure(self):
+        file = request.POST.get('file_upload', None)
+
+        if file is not None and file != '':
+            for line in file.value.split('\n'):
+                if line.strip() == '':
+                    continue
+                line = line.strip().split(',')
+                title = line[1].strip()
+                title_short = line[0].strip().lower()
+                description = line[2].strip()
+                parent = line[3].strip().lower()
+                try:
+                    if parent == '':
+                        tag = meta.Session.query(LocationTag).filter_by(title_short = title_short)\
+                            .filter_by(parent = None).one()
+                    else:
+                        tag = meta.Session.query(LocationTag).filter(LocationTag.title_short==title_short)\
+                            .join('parent_item', aliased=True).filter(LocationTag.title_short==parent)\
+                            .one()
+                except NoResultFound:
+                    tag = LocationTag(title = title,
+                                      title_short = title_short,
+                                      description = description)
+                tag.title = title
+                tag.title_short = title_short
+                tag.description = description
+
+                meta.Session.add(tag)
+                if parent != '':
+                    try:
+                        parent = meta.Session.query(LocationTag).filter_by(title_short=parent).one()
+                        parent.children.append(tag)
+                    except NoResultFound:
+                        continue
+                meta.Session.commit()
+        redirect_to(controller='structure', action='index')
--- src/ututi/templates/admin/import.mako
+++ src/ututi/templates/admin/import.mako
@@ -4,8 +4,15 @@
   <title>UTUTI – student information online</title>
 </%def>
 
-<h1>${_('Data import')}</h1>
+<h1>${_('User import')}</h1>
 <form name="user_import" method="post" action="/admin/import_users" enctype="multipart/form-data">
+      <label for="file_upload">CSV File</label>
+      <input type="file" name="file_upload" id="file_upload"/>
+      <input type="submit" value="Upload" name="Upload"/>
+</form>
+
+<h1>${_('Structure import')}</h1>
+<form name="structure_import" method="post" action="/admin/import_structure" enctype="multipart/form-data">
       <label for="file_upload">CSV File</label>
       <input type="file" name="file_upload" id="file_upload"/>
       <input type="submit" value="Upload" name="Upload"/>
