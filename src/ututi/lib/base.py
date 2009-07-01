ac36fb15ccb041aaaf8482ce32d91fee56293f25
Domas Monkus <domas@port.(none)>
2009-07-01 16:34:47 +0300

    Adding the current user object to all controllers.

--- src/ututi/lib/base.py
+++ src/ututi/lib/base.py
@@ -4,6 +4,7 @@ Provides the BaseController class for subclassing.
 """
 from pylons.controllers import WSGIController
 from pylons.templating import render_mako as render
+from pylons import c
 
 from ututi.model import meta
 
@@ -14,6 +15,8 @@ class BaseController(WSGIController):
         # WSGIController.__call__ dispatches to the Controller method
         # the request is routed to. This routing information is
         # available in environ['pylons.routes_dict']
+        from ututi.lib import current_user
+        c.user = current_user()
         try:
             return WSGIController.__call__(self, environ, start_response)
         finally:
