6966c5656c3809d160e295f85fbddc7184bf38c4
Domas Monkus <domas@port.(none)>
2009-07-10 12:50:21 +0300

    Moved mailing list import tests to level 2.

--- src/ututi/tests/functional/test_all.py
+++ src/ututi/tests/functional/test_all.py
@@ -3,6 +3,6 @@ import unittest
 from ututi.tests.functional import collect_ftests
 
 def test_suite():
-    return unittest.TestSuite([collect_ftests(exclude=["real_import_smoke_test.txt"]),
-                               collect_ftests(filenames=["real_import_smoke_test.txt"],
+    return unittest.TestSuite([collect_ftests(exclude=["real_import_smoke_test.txt", "mailing_list_import.txt"]),
+                               collect_ftests(filenames=["real_import_smoke_test.txt", "mailing_list_import.txt"],
                                               level=2)])
