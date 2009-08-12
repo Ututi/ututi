3a4d6454ff0c4a32a75b99dec2cce3b35f1bd510
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2009-08-12 18:48:32 +0300

    Updated release workflow docs a bit.

--- deployment/setup_users.sql
+++ deployment/setup_users.sql
@@ -10,15 +10,14 @@ sudo -u postgres /usr/lib/postgresql/8.3/bin/createlang plpgsql ututi_release
 bin/paster setup-app deployment/release.ini
 bin/paster setup-app deployment/testing.ini
 */
-CREATE USER u2release WITH NOCREATEDB NOCREATEUSER UNENCRYPTED password 'release';
-CREATE USER u2testing WITH NOCREATEDB NOCREATEUSER UNENCRYPTED password 'testing';
-CREATE DATABASE ututi_release WITH OWNER = u2release ENCODING = 'UTF-8';
-CREATE DATABASE ututi_testing WITH OWNER = u2testing ENCODING = 'UTF-8';
+sudo -u postgres psql -c "CREATE USER u2release WITH NOCREATEDB NOCREATEUSER UNENCRYPTED password 'release'"
+sudo -u postgres psql -c "CREATE USER u2testing WITH NOCREATEDB NOCREATEUSER UNENCRYPTED password 'testing'"
+sudo -u postgres psql -c "CREATE DATABASE ututi_release WITH OWNER = u2release ENCODING = 'UTF-8'"
+sudo -u postgres psql -c "CREATE DATABASE ututi_testing WITH OWNER = u2testing ENCODING = 'UTF-8'"
 
 /*
-sudo -u postgres psql
-DROP DATABASE ututi_release;
-DROP DATABASE ututi_testing;
-DROP USER u2release;
-DROP USER u2testing;
+sudo -u postgres psql -c "DROP DATABASE ututi_release"
+sudo -u postgres psql -c "DROP DATABASE ututi_testing"
+sudo -u postgres psql -c "DROP USER u2release"
+sudo -u postgres psql -c "DROP USER u2testing"
 */
