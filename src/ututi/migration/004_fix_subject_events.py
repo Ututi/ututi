51da7cc0a7895a87ccb273cd6f21166addc2a06e
Domas Monkus <domas@port.nous.lt>
2009-09-15 16:56:56 +0300

    Updated the subject event trigger to avoid creating duplicate subject creation events.

--- src/ututi/migration/004_fix_subject_events.py
+++ src/ututi/migration/004_fix_subject_events.py
@@ -11,10 +11,14 @@ def upgrade(engine):
 CREATE OR REPLACE FUNCTION subject_event_trigger() RETURNS trigger AS $$
     DECLARE
       sid int8 := NULL;
+      eid int8 := NULL;
     BEGIN
       SELECT id INTO sid FROM subjects WHERE subject_id = NEW.subject_id;
       IF NOT FOUND THEN
-          EXECUTE add_event(NEW.id, cast('subject_created' as varchar));
+          SELECT id INTO eid FROM events WHERE object_id = NEW.id AND event_type = 'subject_created';
+          IF NOT FOUND THEN
+              EXECUTE add_event(NEW.id, cast('subject_created' as varchar));
+          END IF;
       ELSE
          EXECUTE add_event(NEW.id, cast('subject_modified' as varchar));
       END IF;
