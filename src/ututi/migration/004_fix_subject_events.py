f7bfdafdd8f6282436b36fc1f8684448d15f6aca
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2009-09-17 17:22:51 +0300

    Formatting fixes for migration scripts made during review.

--- src/ututi/migration/004_fix_subject_events.py
+++ src/ututi/migration/004_fix_subject_events.py
@@ -1,13 +1,29 @@
-# Fix group ids that have slashes or + signs
+"""
+Evolution sctript that fixes subject events.
+
+We had no subject modified event so all the subject modifications were
+firing subject added event.
+
+This script updates the trigger so it would create modification events.
+"""
 
 def update_subject_events(connection, id):
-    """For the subject specified, leave only one creation event, converting others to modification events."""
-    firstevent = list(connection.execute(r"select id from events where event_type = 'subject_created' and object_id = %i order by created desc limit 1" % id))
-    connection.execute(r"update events set event_type = 'subject_modified' where  object_id = %i and event_type = 'subject_created' and id != %i" % (int(id), int(firstevent[0][0])))
+    """For the subject specified, leave only one creation event.
 
-def upgrade(engine):
-    connection = engine.connect()
-    connection.execute(r"""
+    All the other events are converted into modification events.
+    """
+    firstevent = list(connection.execute(r"select id from events"
+                                         " where event_type = 'subject_created'"
+                                         "       and object_id = %i"
+                                         " order by created desc limit 1" % id))
+    connection.execute(r"update events"
+                       " set event_type = 'subject_modified'"
+                       " where object_id = %i"
+                       " and event_type = 'subject_created'"
+                       " and id != %i" % (int(id), int(firstevent[0][0])))
+
+
+update_subject_event_trigger = r"""
 CREATE OR REPLACE FUNCTION subject_event_trigger() RETURNS trigger AS $$
     DECLARE
       sid int8 := NULL;
@@ -30,18 +46,22 @@ DROP TRIGGER subject_event_trigger ON subjects;
 
 CREATE TRIGGER subject_event_trigger BEFORE INSERT OR UPDATE ON subjects
     FOR EACH ROW EXECUTE PROCEDURE subject_event_trigger();
+"""
 
-    """)
+def upgrade(engine):
+    connection = engine.connect()
+    connection.execute(update_subject_event_trigger)
 
     #find occasions where the same subject has several creation events
-    repeats = list(connection.execute(r"select object_id, count(id) from events where event_type = 'subject_created' group by object_id"))
+    repeats = list(connection.execute(r"select object_id, count(id)"
+                                      " from events where event_type = 'subject_created'"
+                                      " group by object_id"))
     for repeat in repeats:
         if repeat[1] > 1:
             update_subject_events(connection, repeat[0])
 
-def downgrade(engine):
-    connection = engine.connect()
-    connection.execute(r"""
+
+revert_subject_event_trigger = r"""
 CREATE OR REPLACE FUNCTION subject_event_trigger() RETURNS trigger AS $$
     BEGIN
       EXECUTE add_event(NEW.id, cast('subject_created' as varchar));
@@ -53,4 +73,8 @@ DROP TRIGGER subject_event_trigger ON subjects;
 
 CREATE TRIGGER subject_event_trigger AFTER INSERT OR UPDATE ON subjects
     FOR EACH ROW EXECUTE PROCEDURE subject_event_trigger();
-    """)
+"""
+
+def downgrade(engine):
+    connection = engine.connect()
+    connection.execute(revert_subject_event_trigger)
--- src/ututi/migration/005_update_file_parents.py
+++ src/ututi/migration/005_update_file_parents.py
@@ -3,63 +3,95 @@
 def upgrade(engine):
     connection = engine.connect()
 
-    connection.execute(r"ALTER TABLE files ADD COLUMN parent_id int8 DEFAULT NULL references content_items(id)")
-    connection.execute(r"ALTER TABLE content_items DROP COLUMN parent_id")
+    connection.execute(r"ALTER TABLE files"
+                       "   ADD COLUMN parent_id int8 DEFAULT NULL references content_items(id)")
+    connection.execute(r"ALTER TABLE content_items"
+                       "   DROP COLUMN parent_id")
 
     #subject files
     files = list(connection.execute(r"select subject_id, file_id from subject_files"))
-    for file in files:
-        connection.execute("update files set parent_id = %i where id = %i" % (file[0], file[1]))
+    for subject_id, file_id in files:
+        connection.execute("update files"
+                           "   set parent_id = %i"
+                           "   where id = %i" % (subject_id, file_id))
 
     #group files
     files = list(connection.execute(r"select group_id, file_id from group_files"))
-    for file in files:
-        connection.execute("update files set parent_id = %i where id = %i" % (file[0], file[1]))
+    for group_id, file_id in files:
+        connection.execute("update files"
+                           "   set parent_id = %i"
+                           "   where id = %i" % (group_id, file_id))
 
     #mailing list attachments
-    files = list(connection.execute(r"select message_id, group_id, file_id from group_mailing_list_attachments"))
-    for file in files:
-        id = list(connection.execute(r"select id from group_mailing_list_messages where group_id = %i and message_id = '%s'" % (file[1], file[0])))
-        connection.execute("update files set parent_id = %i where id = %i" % (id[0][0], file[2]))
+    files = list(connection.execute(r"select message_id, group_id, file_id"
+                                    "   from group_mailing_list_attachments"))
+    for message_id, group_id, file_id in files:
+        id = list(connection.execute(r"select id from group_mailing_list_messages"
+                                     "   where group_id = %i"
+                                     "     and message_id = '%s'" % (group_id,
+                                                                     message_id)))
+        assert len(id) == 1
+        id = id[0][0]
+        connection.execute("update files set parent_id = %i where id = %i" % (id, file_id))
 
 
 def downgrade(engine):
-    connection = engine.connect()
-    connection.execute("SET ututi.active_user TO 1")
-    connection.execute(r"ALTER TABLE content_items ADD COLUMN parent_id int8 DEFAULT NULL references content_items(id)")
-    files = list(connection.execute("select f.id, f.parent_id, c.content_type from files f inner join content_items c on c.id = f.parent_id"))
-    for file in files:
-        if file[2] == 'subject':
-            update_subject(connection, file[1], file[0])
-        elif file[2] == 'group':
-            update_group(connection, file[1], file[0])
-        elif file[2] == 'mailing_list_message':
-            update_message(connection, file[1], file[0])
-        connection.execute("update files set parent_id = NULL where id = %i" % file[0])
-    connection.execute("alter table files drop column parent_id")
+    c = engine.connect()
+    c.execute("SET ututi.active_user TO 1")
+    c.execute(r"ALTER TABLE content_items"
+              "    ADD COLUMN parent_id int8 DEFAULT NULL references content_items(id)")
+    files = list(c.execute("select f.id, f.parent_id, c.content_type"
+                           "   from files f"
+                           "   inner join content_items c on c.id = f.parent_id"))
+    for file_id, file_parent_id, content_type in files:
+        if content_type == 'subject':
+            update_subject(c, file_parent_id, file_id)
+        elif content_type == 'group':
+            update_group(c, file_parent_id, file_id)
+        elif content_type == 'mailing_list_message':
+            update_message(c, file_parent_id, file_id)
+        c.execute("update files set parent_id = NULL where id = %i" % file_id)
+    c.execute("alter table files drop column parent_id")
+
 
 def update_subject(connection, sid, fid):
     """Make sure the file is linked to the subject the old way."""
-    found = list(connection.execute("select * from subject_files where file_id = %i" % fid))
+    found = list(connection.execute("select * from subject_files"
+                                    " where file_id = %i" % fid))
     if len(found) != 0:
-        connection.execute("update subject_files set subject_id = %i where file_id = %i" % (sid, fid))
+        connection.execute("update subject_files set subject_id = %i"
+                           " where file_id = %i" % (sid, fid))
     else:
-        connection.execute("insert into subject_files (subject_id, file_id) values (%i, %i)" % (sid, fid))
+        connection.execute("insert into subject_files (subject_id, file_id)"
+                           " values (%i, %i)" % (sid, fid))
+
 
 def update_group(connection, gid, fid):
-    """Make sure the file is linked to the subject the old way."""
-    found = list(connection.execute("select * from group_files where file_id = %i" % fid))
+    """Make sure the file is linked to the group the old way."""
+    found = list(connection.execute("select * from group_files"
+                                    " where file_id = %i" % fid))
     if len(found) != 0:
-        connection.execute("update group_files set group_id = %i where file_id = %i" % (gid, fid))
+        connection.execute("update group_files set group_id = %i"
+                           " where file_id = %i" % (gid, fid))
     else:
-        connection.execute("insert into group_files (group_id, file_id) values (%i, %i)" % (gid, fid))
+        connection.execute("insert into group_files (group_id, file_id)"
+                           " values (%i, %i)" % (gid, fid))
 
 def update_message(connection, mid, fid):
-    """Make sure the file is linked to the subject the old way."""
-    msg = list(connection.execute("select message_id, group_id from group_mailing_list_messages where id = %i" % mid))
+    """Make sure the file is linked to the message the old way."""
+    msg = list(connection.execute("select message_id, group_id"
+                                  " from group_mailing_list_messages"
+                                  " where id = %i" % mid))
     if len(msg) == 1:
-        found = list(connection.execute("select * from group_mailing_list_attachments where file_id = %i" % fid))
+        message_id, group_id = msg[0]
+        found = list(connection.execute("select *"
+                                        " from group_mailing_list_attachments"
+                                        " where file_id = %i" % fid))
         if len(found) != 0:
-            connection.execute("update group_mailing_list_attachments set message_id = '%s', group_id = %i where file_id = %i" % (msg[0][0], msg[0][1], fid))
+            connection.execute("update group_mailing_list_attachments"
+                               " set message_id = '%s', group_id = %i"
+                               " where file_id = %i" % (message_id, group_id, fid))
         else:
-            connection.execute("insert into group_mailing_list_attachments (message_id, group_id, file_id) values ('%s', %i, %i)" % (msg[0][0], msg[0][1], fid))
+            connection.execute("insert into group_mailing_list_attachments"
+                               " (message_id, group_id, file_id)"
+                               " values ('%s', %i, %i)" % (message_id, group_id, fid))
