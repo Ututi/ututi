31903830674cf4c7f77855cdf2446ae2549160b2
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2009-10-05 01:39:27 +0300

    Added a test for file upload event. Made file upload events fire when file is uploaded.

--- src/ututi/model/defaults.sql
+++ src/ututi/model/defaults.sql
@@ -503,29 +503,17 @@ CREATE TRIGGER subject_page_event_trigger AFTER INSERT OR UPDATE ON subject_page
     FOR EACH ROW EXECUTE PROCEDURE subject_page_event_trigger();;
 
 
-CREATE FUNCTION subject_file_event_trigger() RETURNS trigger AS $$
+CREATE FUNCTION file_event_trigger() RETURNS trigger AS $$
     BEGIN
       INSERT INTO events (object_id, author_id, event_type, file_id)
-             VALUES (NEW.subject_id, cast(current_setting('ututi.active_user') as int8), 'file_uploaded', NEW.file_id);
+             VALUES (NEW.parent_id, cast(current_setting('ututi.active_user') as int8), 'file_uploaded', NEW.id);
       RETURN NEW;
     END
 $$ LANGUAGE plpgsql;;
 
 
-CREATE TRIGGER subject_file_event_trigger AFTER INSERT OR UPDATE ON subject_files
-    FOR EACH ROW EXECUTE PROCEDURE subject_file_event_trigger();;
-
-
-CREATE FUNCTION group_file_event_trigger() RETURNS trigger AS $$
-    BEGIN
-      INSERT INTO events (object_id, author_id, event_type, file_id)
-             VALUES (NEW.group_id, cast(current_setting('ututi.active_user') as int8), 'file_uploaded', NEW.file_id);
-      RETURN NEW;
-    END
-$$ LANGUAGE plpgsql;;
-
-CREATE TRIGGER group_file_event_trigger AFTER INSERT OR UPDATE ON group_files
-    FOR EACH ROW EXECUTE PROCEDURE group_file_event_trigger();;
+CREATE TRIGGER file_event_trigger AFTER INSERT OR UPDATE ON files
+    FOR EACH ROW EXECUTE PROCEDURE file_event_trigger();;
 
 
 CREATE FUNCTION subject_event_trigger() RETURNS trigger AS $$
