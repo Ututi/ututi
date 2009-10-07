b7d78d15d1a55ae37e85bc97d1ca78eebefaa60b
Domas Monkus <domas@port.nous.lt>
2009-10-07 15:26:52 +0300

    Updated search triggers, moving the updating of serach items into separate functions, which can be run separately, when the need arises (hint: soon).

--- src/ututi/model/defaults.sql
+++ src/ututi/model/defaults.sql
@@ -254,44 +254,58 @@ create table search_items (
 
 create index search_items_idx on search_items using gin(terms);;
 
-CREATE FUNCTION update_group_search() RETURNS trigger AS $$
+CREATE FUNCTION update_group_worker(groups) RETURNS void AS $$
     DECLARE
         search_id int8 := NULL;
+        grp ALIAS FOR $1;
     BEGIN
-      SELECT content_item_id INTO search_id  FROM search_items WHERE content_item_id = NEW.id;
+      SELECT content_item_id INTO search_id  FROM search_items WHERE content_item_id = grp.id;
       IF FOUND THEN
-        UPDATE search_items SET terms = to_tsvector(coalesce(NEW.title,''))
-          || to_tsvector(coalesce(NEW.description, ''))
-          || to_tsvector(coalesce(NEW.page, ''))
-          || to_tsvector(coalesce(NEW.group_id, ''))
+        UPDATE search_items SET terms = to_tsvector(coalesce(grp.title,''))
+          || to_tsvector(coalesce(grp.description, ''))
+          || to_tsvector(coalesce(grp.page, ''))
+          || to_tsvector(coalesce(grp.group_id, ''))
            WHERE content_item_id=search_id;
       ELSE
-        INSERT INTO search_items (content_item_id, terms) VALUES (NEW.id,
-          to_tsvector(coalesce(NEW.title,''))
-          || to_tsvector(coalesce(NEW.description, ''))
-          || to_tsvector(coalesce(NEW.group_id, ''))
-          || to_tsvector(coalesce(NEW.page, '')));
+        INSERT INTO search_items (content_item_id, terms) VALUES (grp.id,
+          to_tsvector(coalesce(grp.title,''))
+          || to_tsvector(coalesce(grp.description, ''))
+          || to_tsvector(coalesce(grp.group_id, ''))
+          || to_tsvector(coalesce(grp.page, '')));
       END IF;
+    END
+$$ LANGUAGE plpgsql;;
+
+CREATE FUNCTION update_group_search() RETURNS trigger AS $$
+    BEGIN
+      PERFORM update_group_worker(NEW);
       RETURN NEW;
     END
 $$ LANGUAGE plpgsql;;
 
-
 CREATE TRIGGER update_group_search AFTER INSERT OR UPDATE ON groups
     FOR EACH ROW EXECUTE PROCEDURE update_group_search();;
 
-CREATE FUNCTION update_page_search() RETURNS trigger AS $$
+
+CREATE FUNCTION update_page_worker(page_versions) RETURNS void AS $$
     DECLARE
         search_id int8 := NULL;
+        page ALIAS FOR $1;
     BEGIN
-      SELECT content_item_id INTO search_id  FROM search_items WHERE content_item_id = NEW.page_id;
+      SELECT content_item_id INTO search_id  FROM search_items WHERE content_item_id = page.page_id;
       IF FOUND THEN
-        UPDATE search_items SET terms = to_tsvector(coalesce(NEW.title,''))
-          || to_tsvector(coalesce(NEW.content,'')) WHERE content_item_id=search_id;
+        UPDATE search_items SET terms = to_tsvector(coalesce(page.title,''))
+          || to_tsvector(coalesce(page.content,'')) WHERE content_item_id=search_id;
       ELSE
-        INSERT INTO search_items (content_item_id, terms) VALUES (NEW.page_id,
-          to_tsvector(coalesce(NEW.title,'')) || to_tsvector(coalesce(NEW.content, '')));
+        INSERT INTO search_items (content_item_id, terms) VALUES (page.page_id,
+          to_tsvector(coalesce(page.title,'')) || to_tsvector(coalesce(page.content, '')));
       END IF;
+    END
+$$ LANGUAGE plpgsql;;
+
+CREATE FUNCTION update_page_search() RETURNS trigger AS $$
+    BEGIN
+      PERFORM update_page_worker(NEW);
       RETURN NEW;
     END
 $$ LANGUAGE plpgsql;;
@@ -299,28 +313,37 @@ $$ LANGUAGE plpgsql;;
 CREATE TRIGGER update_page_search AFTER INSERT OR UPDATE ON page_versions
     FOR EACH ROW EXECUTE PROCEDURE update_page_search();;
 
-CREATE FUNCTION update_subject_search() RETURNS trigger AS $$
+
+CREATE FUNCTION update_subject_worker(subjects) RETURNS void AS $$
     DECLARE
         search_id int8 := NULL;
+        subject ALIAS FOR $1;
     BEGIN
-      SELECT content_item_id INTO search_id  FROM search_items WHERE content_item_id = NEW.id;
+      SELECT content_item_id INTO search_id  FROM search_items WHERE content_item_id = subject.id;
       IF FOUND THEN
-        UPDATE search_items SET terms = to_tsvector(coalesce(NEW.title,''))
-          || to_tsvector(coalesce(NEW.lecturer,''))
-          || to_tsvector(coalesce(NEW.description,''))
+        UPDATE search_items SET terms = to_tsvector(coalesce(subject.title,''))
+          || to_tsvector(coalesce(subject.lecturer,''))
+          || to_tsvector(coalesce(subject.description,''))
           WHERE content_item_id=search_id;
       ELSE
-        INSERT INTO search_items (content_item_id, terms) VALUES (NEW.id,
-          to_tsvector(coalesce(NEW.title,''))
-          || to_tsvector(coalesce(NEW.description,''))
-          || to_tsvector(coalesce(NEW.lecturer, '')));
+        INSERT INTO search_items (content_item_id, terms) VALUES (subject.id,
+          to_tsvector(coalesce(subject.title,''))
+          || to_tsvector(coalesce(subject.description,''))
+          || to_tsvector(coalesce(subject.lecturer, '')));
       END IF;
-      RETURN NEW;
+    END
+$$ LANGUAGE plpgsql;;
+
+CREATE FUNCTION update_subject_search() RETURNS trigger AS $$
+    BEGIN
+        PERFORM update_subject_worker(NEW);
+        RETURN NEW;
     END
 $$ LANGUAGE plpgsql;;
 
 CREATE TRIGGER update_subject_search AFTER INSERT OR UPDATE ON subjects
     FOR EACH ROW EXECUTE PROCEDURE update_subject_search();;
+
 
 /* A table for connecting tags and the tagged content */
 create table content_tags (id bigserial not null,
