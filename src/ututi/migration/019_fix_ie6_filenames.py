7247702bba3846c70deb98c9c872db4410a13dc5
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2009-10-19 18:46:33 +0300

    Only update files that need fixing, so we would not generate so many events.

--- src/ututi/migration/019_fix_ie6_filenames.py
+++ src/ututi/migration/019_fix_ie6_filenames.py
@@ -20,13 +20,17 @@ def upgrade(engine):
     for id, filename, title, who in files:
         engine.echo = True
         c.execute(text("SET ututi.active_user TO :uid"), uid=who)
-        c.execute(text("update files"
-                       "   set title = :title,"
-                       "   filename  = :filename"
-                       "   where id  = :id"),
-                  title=cleanupFileName(title),
-                  filename=cleanupFileName(filename),
-                  id=id)
+        new_filename = cleanupFileName(filename)
+        new_title = cleanupFileName(title)
+        if (new_filename != filename or
+            new_title != title):
+            c.execute(text("update files"
+                           "   set title = :title,"
+                           "   filename  = :filename"
+                           "   where id  = :id"),
+                      title=new_title,
+                      filename=new_filename,
+                      id=id)
 
 
 def downgrade(engine):
