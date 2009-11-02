ffa86ebaa377dd7c26351aedc022d755a788efce
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2009-11-02 02:38:04 +0200

    Change the way news are gathered and sent.

--- src/ututi/controllers/news.py
+++ src/ututi/controllers/news.py
@@ -13,34 +13,50 @@ from ututi.model.events import FileUploadedEvent
 from ututi.model.events import PageCreatedEvent
 from ututi.model.events import PageModifiedEvent
 from ututi.model.events import Event
+from ututi.model import Subject
+from ututi.model import Group
+from ututi.model import GroupMember
+from ututi.model import UserSubjectMonitoring
 from ututi.model import User, meta
 
 
 class NewsController(BaseController):
 
-    def _events(self, user, from_time, to_time):
+    def _all_events(self, from_time, to_time):
         events = meta.Session.query(Event)\
-            .filter(or_(Event.object_id.in_([s.id
-                                             for s in user.all_watched_subjects]),
-                        Event.object_id.in_([m.group.id
-                                             for m in user.memberships])))\
-            .filter(Event.author_id != user.id)\
             .filter(Event.created < to_time)\
             .filter(Event.created >= from_time)\
             .filter(or_(Event.file_id != None,
                         Event.page_id != None))\
             .order_by(desc(Event.created))\
             .all()
-
-
         return events
 
-    def _users_for_hourly_news(self):
-        return meta.Session.query(User).filter_by(receive_email_each='hour')
+    def _group_recipients(self, group, period):
+        recipients = meta.Session.query(GroupMember).\
+            filter_by(group=group, receive_email_each=period).all()
+        return [recipient.user for recipient in recipients]
 
-    def _users_for_daily_news(self):        return meta.Session.query(User).filter_by(receive_email_each='day')
+    def _subject_recipients(self, subject, period):
+        all_recipients = []
+        groups =  meta.Session.query(Group).filter(Group.watched_subjects.contains(subject)).all()
+        for group in groups:
+            all_recipients.extend(self._group_recipients(group, period))
 
-    def range(self, days=0, hours = 0):
+        usms = meta.Session.query(UserSubjectMonitoring).\
+            filter(UserSubjectMonitoring.subject==subject).\
+            filter(User.receive_email_each==period).all()
+        recipients = [usm.user for usm in usms]
+        all_recipients.extend(recipients)
+        return list(set(all_recipients))
+
+    def _recipients(self, event, period):
+        if isinstance(event.context, Subject):
+            return self._subject_recipients(event.context, period)
+        if isinstance(event.context, Group):
+            return self._group_recipients(event.context, period)
+
+    def range(self, days=0, hours=0):
         date = request.params.get('date')
         date = parse_date(date)
 
@@ -95,10 +111,19 @@ class NewsController(BaseController):
         sections = sorted(sections.items())
         return [section for (id, section) in sections]
 
-    def _send_news(self, users, **kwargs):
+    def _send_news(self, period, **kwargs):
         dtstart, dtend = self.range(**kwargs)
-        for user in users:
-            events = self._events(user, dtstart, dtend)
+
+        recipient_to_events = {}
+        for event in self._all_events(dtstart, dtend):
+            recipients = self._recipients(event, period)
+            for recipient in recipients:
+                events = recipient_to_events.setdefault(recipient.id, [])
+                events.append(event)
+        for uid, events in recipient_to_events.items():
+            events = [ev for ev in events
+                      if ev.user.id != uid]
+
             if not events:
                 continue
             sections = self._get_sections(events)
@@ -110,10 +135,10 @@ class NewsController(BaseController):
             html = render('/emails/news_html.mako',
                           extra_vars=extra_vars)
             msg = Message(subject, text, html)
-            user.send(msg)
+            user = User.get_byid(uid).send(msg)
 
     def hourly(self):
-        self._send_news(self._users_for_hourly_news(), hours=1)
+        self._send_news('hour', hours=1)
 
     def daily(self):
-        self._send_news(self._users_for_daily_news(), days=1)
+        self._send_news('day', days=1)
