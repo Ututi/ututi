79bf1e44fb8ffed6c1fd1c4630d78521f82df7b3
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2009-11-10 23:07:13 +0200

    Enable language specific template switching.

--- src/ututi/controllers/home.py
+++ src/ututi/controllers/home.py
@@ -138,7 +138,7 @@ class HomeController(UniversityListMixin):
             if request.params.has_key('js'):
                 return render_mako_def('/anonymous_index/lt.mako','universities', unis=c.unis, ajax_url=url(controller='home', action='index'))
             c.slideshow = request.params.has_key('slide')
-            return render_lang('/anonymous_index/lt.mako')
+            return render_lang('/anonymous_index.mako')
 
     def about(self):
         return render('/about/%s.mako' % c.lang)
@@ -225,7 +225,7 @@ class HomeController(UniversityListMixin):
                 return render('/login.mako')
 
             self._get_unis()
-            return render_lang('/anonymous_index/lt.mako')
+            return render_lang('/anonymous_index.mako')
 
     def _pswrecovery_form(self):
         return render('home/recoveryform.mako')
