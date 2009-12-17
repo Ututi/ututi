81311569f8f8b4e96b706685981787d7d8d18829
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2009-12-17 18:50:13 +0200

    Ooops, forgot to increment the evolution number. Again.

--- src/ututi/migration/__init__.py
+++ src/ututi/migration/__init__.py
@@ -34,7 +34,7 @@ class EvolutionScript(object):
 
 class GreatMigrator(object):
 
-    min_version = 31
+    min_version = 32
 
     def __init__(self, engine, language=None):
         self.engine = engine
