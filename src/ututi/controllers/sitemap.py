adccad0e01746ecba81213f59d6434b2446d55e8
Domas Monkus <domas@port.nous.lt>
2009-12-17 14:20:18 +0200

    Working around the cache sitemap in tests.

--- src/ututi/controllers/sitemap.py
+++ src/ututi/controllers/sitemap.py
@@ -10,5 +10,13 @@ class SitemapController(BaseController):
         c.schools = meta.Session.query(LocationTag).filter(LocationTag.parent == None).order_by(LocationTag.title_short.asc()).all()
         c.subjects = meta.Session.query(Subject).filter_by(deleted_by=None).order_by(Subject.title.asc()).all()
         c.groups = meta.Session.query(Group).filter_by(deleted_by=None).order_by(Group.title.asc()).all()
-        return render('sitemap/index.mako',
-                      cache_key='sitemap', cache_expire=7*24*3600, cache_type='file')
+
+        cache = int(config.get('cache_lifetime', 7*24*3600))
+
+        if cache != 0:
+            return render('sitemap/index.mako',
+                          cache_key='sitemap',
+                          cache_expire=cache,
+                          cache_type='file')
+        else:
+            return render('sitemap/index.mako')
