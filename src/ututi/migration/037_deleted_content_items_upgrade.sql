122dcfffc888cf1b54077d520b90427a59ada966
Domas Monkus <domas@port.nous.lt>
2010-03-07 12:25:22 +0200

    Updated deletion trigger to undelete properly. Added this to the migration.

--- src/ututi/migration/037_deleted_content_items_upgrade.sql
+++ src/ututi/migration/037_deleted_content_items_upgrade.sql
@@ -1,1 +1,14 @@
 update content_items set deleted_on = null where deleted_by is null and deleted_on is not null;
+
+CREATE OR REPLACE FUNCTION set_deleted_on() RETURNS trigger AS $$
+    BEGIN
+        IF not NEW.deleted_by is NULL AND OLD.deleted_by is NULL THEN
+          NEW.deleted_on := (now() at time zone 'UTC');
+        ELSE IF NEW.deleted_by is NULL AND NOT OLD.deleted_by is NULL THEN
+          NEW.deleted_on := NULL;
+        END IF;
+
+        RETURN NEW;
+    END
+$$ LANGUAGE plpgsql;;
+
