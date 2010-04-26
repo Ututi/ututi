a6d4df3db267d806dd2b07ed1d094f90402090b6
Gintautas Miliauskas <gintautas@miliauskas.lt>
2010-04-26 18:02:35 +0300

    Unique index for medals, awarded_on timestamp tracking.

--- src/ututi/model/defaults.sql
+++ src/ututi/model/defaults.sql
@@ -66,7 +66,9 @@ create table user_medals (
        id bigserial not null,
        user_id int8 default null references users(id),
        medal_type varchar(30) not null,
-       primary key (id));;
+       awarded_on timestamp not null default (now() at time zone 'UTC'),
+       primary key (id),
+       unique(user_id, medal_type));;
 
 insert into user_medals (user_id, medal_type) values (1, 'admin2');
 
