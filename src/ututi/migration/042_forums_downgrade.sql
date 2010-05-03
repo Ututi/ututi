86c9cde10a8139c1af1fc0d0c4228341e9126a70
Gintautas Miliauskas <gintautas@miliauskas.lt>
2010-05-03 17:00:52 +0300

    Fixed forums downgrade script.

--- src/ututi/migration/042_forums_downgrade.sql
+++ src/ututi/migration/042_forums_downgrade.sql
@@ -1,6 +1,10 @@
-alter table groups drop column mailinglist_enabled;
+ALTER TABLE groups DROP COLUMN mailinglist_enabled;
 
-ALTER TABLE forum_posts ALTER COLUMN category_id TYPE varchar(100) default null USING
+ALTER TABLE forum_posts ADD COLUMN forum_id varchar(100) DEFAULT NULL;
+
+UPDATE forum_posts SET forum_id =
     CASE category_id WHEN 1 THEN 'community' WHEN 2 THEN 'bugs' END;
+
+ALTER TABLE forum_posts DROP COLUMN category_id;
 
 DROP TABLE forum_categories;
