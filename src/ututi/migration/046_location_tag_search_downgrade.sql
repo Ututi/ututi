1eb9d72c28f914423f0d85f5ba9850d52cf699aa
Domas Monkus <domas@port.nous.lt>
2010-05-11 16:29:58 +0300

    Added indexing table and triggers for location tag search.

---
+++ src/ututi/migration/046_location_tag_search_downgrade.sql
@@ -0,0 +1,3 @@
+DROP TABLE tag_search_items;
+
+DROP TRIGGER update_tag_search ON tags;
--- src/ututi/model/defaults.sql
+++ src/ututi/model/defaults.sql
@@ -788,3 +788,67 @@ create table payments (
        raw_payent_type varchar(250), -- deprecated
 
        primary key (id));;
+
+/* tag search */
+CREATE TABLE tag_search_items (
+       tag_id int8 NOT NULL REFERENCES tags(id) ON DELETE CASCADE,
+       terms tsvector,
+       PRIMARY KEY (tag_id));;
+
+CREATE index tag_search_idx ON tag_search_items USING gin(terms);;
+
+CREATE OR REPLACE FUNCTION make_vector(attrs text[]) RETURNS tsvector AS $$
+    DECLARE
+      str text := NULL;
+      vector tsvector := NULL;
+    BEGIN
+      SELECT coalesce(array_to_string($1, ' '), '') INTO str;
+      SELECT to_tsvector(str) INTO vector;
+      RETURN vector;
+    END
+$$ LANGUAGE plpgsql;;
+
+CREATE OR REPLACE FUNCTION tag_indexable_content(int8) RETURNS tsvector AS $$
+    DECLARE
+        tag_id ALIAS FOR $1;
+        tag tags;
+        vector tsvector := NULL;
+    BEGIN
+        SELECT * FROM tags INTO tag WHERE id = tag_id AND tag_type = 'location';
+        IF FOUND AND NOT tag_id IS NULL THEN
+          SELECT make_vector(ARRAY[tag.title, tag.title_short, tag.description, tag.site_url]) INTO vector;
+          RETURN vector;
+        ELSE
+          RETURN to_tsvector('');
+        END IF;
+    END
+$$ LANGUAGE plpgsql;;
+
+CREATE OR REPLACE FUNCTION update_tag_worker(tags) RETURNS void AS $$
+    DECLARE
+        search_id int8 := NULL;
+        tag ALIAS FOR $1;
+        vector tsvector := NULL;
+    BEGIN
+      SELECT INTO vector tag_indexable_content(tags.id) || tag_indexable_content(tags.parent_id) FROM tags WHERE id = tag.id;
+      SELECT tag_id INTO search_id FROM tag_search_items WHERE tag_id = tag.id;
+
+      IF FOUND and tag.tag_type = 'location' THEN
+        UPDATE tag_search_items SET terms = vector
+          WHERE tag_id=search_id;
+      ELSE
+        INSERT INTO tag_search_items (tag_id, terms) VALUES (tag.id, vector);
+      END IF;
+    END
+$$ LANGUAGE plpgsql;;
+
+CREATE OR REPLACE FUNCTION update_tag_search() RETURNS trigger AS $$
+    BEGIN
+      PERFORM update_tag_worker(NEW);
+      RETURN NEW;
+    END
+$$ LANGUAGE plpgsql;;
+
+CREATE TRIGGER update_tag_search AFTER INSERT OR UPDATE ON tags
+    FOR EACH ROW EXECUTE PROCEDURE update_tag_search();;
+
