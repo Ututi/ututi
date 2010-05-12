1226978ecfee9bd0dd685e0b4ada4aea3478b410
Gintautas Miliauskas <gintautas@miliauskas.lt>
2010-05-12 16:44:35 +0300

    Do not track public forum post events.

--- src/ututi/migration/048_forum_post_events_upgrade.sql
+++ src/ututi/migration/048_forum_post_events_upgrade.sql
@@ -5,12 +5,14 @@ UPDATE events SET event_type = 'mailinglist_post_created' WHERE event_type = 'fo
 
 CREATE FUNCTION group_forum_message_event_trigger() RETURNS trigger AS $$
     BEGIN
-      INSERT INTO events (object_id, author_id, event_type, post_id)
-             VALUES (
-                (SELECT group_id FROM forum_categories
-                 WHERE forum_categories.id = NEW.category_id),
-                cast(current_setting('ututi.active_user') as int8),
-                'forum_post_created', NEW.id);
+      IF NEW.category_id > 2 THEN   -- group forum
+        INSERT INTO events (object_id, author_id, event_type, post_id)
+               VALUES (
+                  (SELECT group_id FROM forum_categories
+                   WHERE forum_categories.id = NEW.category_id),
+                  cast(current_setting('ututi.active_user') as int8),
+                  'forum_post_created', NEW.id);
+      END IF;
       RETURN NEW;
     END
 $$ LANGUAGE plpgsql;;
--- src/ututi/model/defaults.sql
+++ src/ututi/model/defaults.sql
@@ -693,12 +693,14 @@ CREATE TRIGGER group_mailing_list_message_event_trigger AFTER INSERT OR UPDATE O
 
 CREATE FUNCTION group_forum_message_event_trigger() RETURNS trigger AS $$
     BEGIN
-      INSERT INTO events (object_id, author_id, event_type, post_id)
-             VALUES (
-                (SELECT group_id FROM forum_categories
-                 WHERE forum_categories.id = NEW.category_id),
-                cast(current_setting('ututi.active_user') as int8),
-                'forum_post_created', NEW.id);
+      IF NEW.category_id > 2 THEN   -- group forum
+        INSERT INTO events (object_id, author_id, event_type, post_id)
+               VALUES (
+                  (SELECT group_id FROM forum_categories
+                   WHERE forum_categories.id = NEW.category_id),
+                  cast(current_setting('ututi.active_user') as int8),
+                  'forum_post_created', NEW.id);
+      END IF;
       RETURN NEW;
     END
 $$ LANGUAGE plpgsql;;
