629c01645166b6d2322597845152b5122b5ed9b1
Gintautas Miliauskas <gintautas@miliauskas.lt>
2010-05-17 17:21:05 +0300

    Revert "Mark all existing forum posts as read."  We changed our mind.
    This reverts commit 179e2d6faae749b5ff4063b036b3d2e1591fe836.

--- src/ututi/migration/044_forum_lastseen_upgrade.sql
+++ src/ututi/migration/044_forum_lastseen_upgrade.sql
@@ -3,8 +3,3 @@ CREATE TABLE seen_threads (
        user_id int8 not null references users(id),
        visited_on timestamp not null default '2000-01-01',
        primary key(thread_id, user_id));;
-
-INSERT INTO seen_threads (
-    SELECT DISTINCT forum_posts.thread_id AS thread_id, users.id AS user_id
-    FROM users, forum_posts
-);
