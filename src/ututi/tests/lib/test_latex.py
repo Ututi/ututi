5ee9ba313ea85ad85ab432f2a1aa03998fafff94
Gintautas Miliauskas <gintautas@miliauskas.lt>
2010-06-16 18:01:30 +0300

    Fixed tests.

--- src/ututi/tests/lib/test_latex.py
+++ src/ututi/tests/lib/test_latex.py
@@ -56,7 +56,7 @@ def test_replace_between_latex_and_html():
     service does not support them and so will display incorrectly):
 
         >>> replace_latex_to_html(u'\u0126ello, $$\u017eeme$$!')
-        u'\u0126ello, <img class="latex" alt="\u017eeme" src="http://l.wordpress.com/latex.php?bg=ffffff&amp;fg=000000&amp;s=0&amp;latex=%5Cdisplaystyle+%C5%BEeme" />!'
+        literal(u'\u0126ello, <img class="latex" alt="\u017eeme" src="http://l.wordpress.com/latex.php?bg=ffffff&amp;fg=000000&amp;s=0&amp;latex=%5Cdisplaystyle+%C5%BEeme" />!')
 
     Emtpy strings should stay empty:
 
