571587632ed5d0d954c03f95489081d40c1d4c95
Gintautas Miliauskas <gintautas@miliauskas.lt>
2010-09-01 15:12:10 +0300

    Randomized cache expiration times to spread out cache reloads.

--- src/ututi/lib/cache.py
+++ src/ututi/lib/cache.py
@@ -1,5 +1,11 @@
+import random
 from pylons.decorators.cache import beaker_cache
 
+
 def u_cache(**kwargs):
+    if 'expire' in kwargs:
+        # Vary expiration times by +/- 20%.
+        factor = 0.8 + (random.random() * 0.4)
+        kwargs['expire'] = int(kwargs['expire'] * factor)
     kwargs['cache_response'] = False
     return beaker_cache(**kwargs)
