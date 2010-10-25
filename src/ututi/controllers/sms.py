5e32676235195e584085e9b59f153d104d99bd57
Domas Monkus <domas@monkus.lt>
2010-10-25 12:46:12 +0300

    SMS delivery time - set in utc.

--- src/ututi/controllers/sms.py
+++ src/ututi/controllers/sms.py
@@ -45,5 +45,5 @@ class SmsController(BaseController):
         sms_id = request.params.get('id')
         sms = SMS.get(sms_id)
         sms.delivery_status = request.params.get('status')
-        sms.delivered = datetime.fromtimestamp(float(request.params.get('time')))
+        sms.delivered = datetime.utcfromtimestamp(float(request.params.get('time')))
         meta.Session.commit()
