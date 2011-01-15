e0edd31fbcab0857da5685b6ea11853928f59425
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-01-15 18:32:07 +0200

    PEP 8.

--- src/ututi/controllers/profile/wall.py
+++ src/ututi/controllers/profile/wall.py
@@ -4,6 +4,7 @@ from ututi.lib.wall import WallMixin
 from ututi.lib.fileview import FileViewMixin
 
 class ProfileWallController(WallMixin, FileViewMixin):
+
     def _redirect_url(self):
         """This is the default redirect url of wall methods.
            Subclasses should override it."""
