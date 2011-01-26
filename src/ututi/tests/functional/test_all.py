1f381120b6c67b8ef65013df9dfb784f1aeb7cc0
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2011-01-26 15:08:15 +0200

    Separate test layers into separate directories.

--- src/ututi/tests/functional/test_all.py
+++ src/ututi/tests/functional/test_all.py
@@ -1,10 +1,13 @@
 import unittest
 
+import ututi.tests.functional.errors
+import ututi.tests.functional.slow
 from ututi.tests import UtutiErrorsLayer
 from ututi.tests.functional import collect_ftests
 
 def test_suite():
-    return unittest.TestSuite([collect_ftests(exclude=["real_import_smoke_test.txt", "errors.txt"]),
-                               collect_ftests(filenames=["errors.txt"], layer=UtutiErrorsLayer),
-                               collect_ftests(filenames=["real_import_smoke_test.txt"],
+    return unittest.TestSuite([collect_ftests(),
+                               collect_ftests(package=ututi.tests.functional.errors,
+                                              layer=UtutiErrorsLayer),
+                               collect_ftests(package=ututi.tests.functional.slow,
                                               level=2)])
