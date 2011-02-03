f8aa3f471d0a3acf703853b6241fa580d0da34d9
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-02-03 12:37:51 +0200

    Add user's error message to "kick monkeys" email text - let's not lose it.

--- src/ututi/controllers/error.py
+++ src/ututi/controllers/error.py
@@ -45,14 +45,15 @@ class ErrorController(SearchController):
         if request.method == 'POST':
             sender = config.get('ututi_emails_from', 'info@ututi.lt')
             action = request.POST.get('submit')
+            user_shout = request.POST.get('error_message')
             message = ""
             if c.user:
                 sender = c.user.emails[0].email
             if action == "shout":
                 h.flash(_('Monkeys are ashamed of what you said and are now working even harder to fix the problem. Until they do that, try to search for something else.'))
-                message = request.POST.get('error_message')
+                message = user_shout
             elif action == "kick":
-                message = _("User kicked monkeys")
+                message = '%s\n\n%s' % (_("User kicked monkeys"), user_shout)
                 h.flash(_('Ouch! Monkeys were kicked and are trying to work harder.  Until they fix this, try to search for something else.'))
             send_email(sender, config.get('error_email', 'errors@ututi.lt'), "["+_("Error message")+"]", message)
         redirect(url(controller='search', action="index"))
