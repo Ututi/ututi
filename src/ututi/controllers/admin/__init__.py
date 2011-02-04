e96813f3673f24dd124307720ccb956c6dc3c3d8
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-02-04 11:57:02 +0200

    Removed old import code and views.

--- src/ututi/controllers/admin/__init__.py
+++ src/ututi/controllers/admin/__init__.py
@@ -1,6 +1,4 @@
 # -*- encoding: utf-8 -*-
-import csv
-import os
 import logging
 import datetime
 
@@ -9,7 +7,6 @@ from pylons.controllers.util import redirect
 from sqlalchemy.sql.expression import or_
 from sqlalchemy.sql.expression import desc
 from sqlalchemy import func
-from datetime import date
 from webhelpers import paginate
 from formencode import htmlfill
 from formencode.compound import Any
@@ -34,11 +31,9 @@ from ututi.lib.emails import teacher_confirmed_email
 from ututi.model.users import AdminUser
 from ututi.model.events import Event
 from ututi.model import Department
-from ututi.model import Region
 from ututi.model import FileDownload
-from ututi.model import SimpleTag
 from ututi.model import SMS, GroupCoupon
-from ututi.model import (meta, User, Email, LocationTag, Group, Subject,
+from ututi.model import (meta, User, Email, Group, Subject,
                          File, PrivateMessage, Teacher)
 from ututi.model import Notification, City, ScienceType, SchoolGrade, BookType
 from ututi.controllers.admin.export import UniversityExportMixin
@@ -48,26 +43,6 @@ from ututi.lib import helpers as h
 
 
 log = logging.getLogger(__name__)
-
-email_map = {'inf@mytutor.lt'             : 'info@mytutor.lt',
-             'indremilukaite@gmai.com'    : 'indremilukaite@gmail.com',
-             'svaras456@gmai.com'         : 'svaras456@gmail.com',
-             'veju.mote@gmsil.com'        : 'dainius1989@gmail.com',
-             'dainius1989@gnail.com'      : 'veju.mote@gmail.com',
-             'jurgis.pragauskis@gmail.com': 'jurgis.pralgauskis@gmail.com',
-             'jarekass+petras@takas.lt'   : '',
-             'asta.samulionyte@khf.vu.lt' : '',
-             'jana89@one.lt'              : '',
-             'info@ututi.lt'              : '',
-             'ted.bing@gmail.com'         : ''}
-
-
-def store_file(file_obj, file_name):
-    path = os.environ.get("upload_import_path", None)
-    if path is not None:
-        file_obj.store(open(os.path.join(path, file_name)))
-    else:
-        file_obj.store('Whatever!')
 
 
 class SMSForm(Schema):
@@ -138,36 +113,9 @@ class AdminController(BaseController, UniversityExportMixin):
         return [[column.strip().decode('utf-8') for column in row]
                 for row in rows]
 
-    def _getReader(self):
-        file = request.POST.get('file_upload', None)
-        if file is not None and file != '':
-            return self._stripAndDecode(csv.reader(file.value.splitlines()))
-        return []
-
-    def _getLines(self):
-        """CSV parser especially for logo import.
-
-        We need to split the logo lines ourselves - csv reader is
-        written in C and can't handle very very long lines.
-
-        Only ids and logos are in the file, so we can assume no quoted
-        cells in the csv file.
-        """
-        file = request.POST.get('file_upload', None)
-
-        if file is not None and file != '':
-            return self._stripAndDecode(
-                [line.split(',')
-                 for line in file.value.splitlines()])
-        return []
-
     @ActionProtector("root")
     def index(self):
         return render('/admin/dashboard.mako')
-
-    @ActionProtector("root")
-    def import_csv(self):
-        return render('/admin/import.mako')
 
     @ActionProtector("root")
     def users(self):
@@ -249,124 +197,6 @@ class AdminController(BaseController, UniversityExportMixin):
             from_time=c.from_time_str,
             to_time=c.to_time_str)
         return render('/admin/users.mako')
-
-    @ActionProtector("root")
-    def import_users(self):
-        for line in self._getReader():
-            fullname = line[2]
-            password = line[1][6:]
-            email = line[3].lower()
-            email = email_map.get(email, email)
-            if not email:
-                continue
-            user = User.get(email)
-            if user is None:
-                user = User(fullname, password, False)
-                email = Email(email)
-                user.emails = [email]
-                email.confirmed = True
-                meta.Session.add(user)
-
-            meta.Session.commit()
-        redirect(url(controller='admin', action='users'))
-
-    @ActionProtector("root")
-    def import_group_members(self):
-        for line in self._getReader():
-            group_id = line[0]
-            user_id = line[1].lower()
-            user_id = email_map.get(user_id, user_id)
-            is_admin = line[2] == u'True'
-
-            user = User.get(user_id)
-            group = Group.get(group_id)
-            if group is not None and user is not None:
-                group.add_member(user, is_admin)
-
-            meta.Session.commit()
-        redirect(url(controller='admin', action='import_csv'))
-
-    @ActionProtector("root")
-    def import_structure(self):
-        for line in self._getReader():
-            title_short = line[0]
-            title = line[1]
-            description = line[2]
-            parent = line[3]
-            region_title = line[4]
-            site_url = line[5]
-
-            tag = LocationTag.get([parent, title_short])
-            if tag is None:
-                tag = LocationTag(title=title,
-                                  title_short=title_short,
-                                  description=description)
-            tag.title = title
-            tag.title_short = title_short
-            tag.description = description
-            tag.site_url = site_url
-            if region_title:
-                tag.region = meta.Session.query(Region).filter_by(title=region_title).one()
-
-            meta.Session.add(tag)
-            if parent:
-                parent = LocationTag.get([parent])
-                parent.children.append(tag)
-            meta.Session.commit()
-        redirect(url(controller='structure', action='index'))
-
-    @ActionProtector("root")
-    def import_groups(self):
-        for row in self._getReader():
-            uni_id = row[5]
-            fac_id = row[4]
-
-            id, title, desc, year = row[:4]
-            group = Group.get(id)
-            if group is None:
-                group = Group(group_id=id)
-                meta.Session.add(group)
-
-            group.title = title
-            group.description = desc
-            group.location = LocationTag.get([uni_id, fac_id])
-
-            if year != '' and year != 'None':
-                group.year = date(int(year), 1, 1)
-            else:
-                group.year = date(2008, 1, 1)
-
-        meta.Session.commit()
-        redirect(url(controller='admin', action='groups'))
-
-    @ActionProtector("root")
-    def import_subjects_without_ids(self):
-        for row in self._getReader():
-            title, lecturer = row[:2]
-            location_path = reversed(row[2:4])
-            tags = filter(bool, [tag.strip() for tag in row[5].split(',')])
-            description = row[-2]
-            location = LocationTag.get(location_path)
-
-            #do not import duplicates
-            existing = meta.Session.query(Subject).filter(Subject.location_id == location.id).filter(func.upper(Subject.title) == func.upper(title)).first()
-            if existing:
-                log.info('Subject exists: %s' % row)
-                h.flash('Subject exists: %s' % row)
-                continue
-
-            title = title
-            lecturer = lecturer
-            subj = Subject('', title, location)
-            subj.lecturer = lecturer
-            subj.description = description
-            meta.Session.add(subj)
-            meta.Session.flush()
-            subj.subject_id = subj.generate_new_id()
-            for tag in tags:
-                subj.tags.append(SimpleTag.get(tag))
-        meta.Session.commit()
-        redirect(url(controller='admin', action='subjects'))
 
     @ActionProtector('root')
     def files(self):
@@ -603,7 +433,6 @@ class AdminController(BaseController, UniversityExportMixin):
         meta.Session.delete(school_grade)
         meta.Session.commit()
         redirect(url(controller="admin", action="school_grades"))
-
 
     @ActionProtector("root")
     @validate(schema=SchoolGradeForm, form='school_grade')
