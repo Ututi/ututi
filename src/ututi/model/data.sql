53526d72a8b0248dbb69f8368930515bbd3ca1fb
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-02-11 16:51:54 +0200

    Added registration entry for easy click-testing.

--- src/ututi/model/data.sql
+++ src/ututi/model/data.sql
@@ -4,3 +4,6 @@ insert into tags (title, title_short, description, parent_id, tag_type)
        values ('D-epartment', 'd', '', 1, 'location');
 insert into users (location_id, username, fullname, password) values (1, 'admin@uni.ututi.com', 'Administrator of the university', 'xnIVufqLhFFcgX+XjkkwGbrY6kBBk0vvwjA7');
 insert into emails (id, email, confirmed) values (1, 'admin@uni.ututi.com', true);
+
+insert into user_registrations (location_id, hash, email)
+       values (1, 'test', 'user@example.com')
