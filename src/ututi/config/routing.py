cbf88e8d560e4e3777e3c6a4ee429db667f25023
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-02-28 10:59:37 +0200

    Removed dead route.

--- src/ututi/config/routing.py
+++ src/ututi/config/routing.py
@@ -231,7 +231,6 @@ def make_map(config):
     map.connect('/home/subjects', controller='profile', action='my_subjects')
 
     #user registration path
-    map.connect('/welcome', controller='profile', action='welcome')
     map.connect('/register/welcome', controller='profile', action='register_welcome')
 
     #new user registration path
