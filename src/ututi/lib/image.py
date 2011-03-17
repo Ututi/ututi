5b885a02c0305f84ca19bb13f6699bdc1a95c398
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-03-17 19:11:19 +0200

    Serving squared logos.

--- src/ututi/lib/image.py
+++ src/ututi/lib/image.py
@@ -12,11 +12,11 @@ from ututi.model import Group, User, LocationTag, Book, UserRegistration
 
 
 def serve_logo(obj_type, obj_id=None, width=None, height=None,
-               default_img_path=None, cache=True):
+               default_img_path=None, cache=True, square=False):
     if cache:
-        img_data = prepare_logo_cached(obj_type, obj_id, width, height, default_img_path)
+        img_data = prepare_logo_cached(obj_type, obj_id, width, height, default_img_path, square)
     else:
-        img_data = prepare_logo(obj_type, obj_id, width, height, default_img_path)
+        img_data = prepare_logo(obj_type, obj_id, width, height, default_img_path, square)
 
     if img_data is None:
         abort(404)
@@ -34,11 +34,11 @@ def serve_logo(obj_type, obj_id=None, width=None, height=None,
 
 
 @u_cache(expire=3600, query_args=False, invalidate_on_startup=True)
-def prepare_logo_cached(obj_type, obj_id, width=None, height=None, default_img_path=None):
-    return prepare_logo(obj_type, obj_id, width=width, height=height, default_img_path=default_img_path)
+def prepare_logo_cached(obj_type, obj_id, width=None, height=None, default_img_path=None, square=False):
+    return prepare_logo(obj_type, obj_id, width=width, height=height, default_img_path=default_img_path, square=square)
 
 
-def prepare_logo(obj_type, obj_id, width=None, height=None, default_img_path=None):
+def prepare_logo(obj_type, obj_id, width=None, height=None, default_img_path=None, square=False):
     obj = None
     if obj_id is not None:
         if obj_type == 'user':
@@ -48,17 +48,19 @@ def prepare_logo(obj_type, obj_id, width=None, height=None, default_img_path=Non
             obj = obj_cls.get(obj_id)
 
     if obj is not None and obj.has_logo():
-        return prepare_image(obj.logo, width, height)
+        return prepare_image(obj.logo, width, height, square)
     elif default_img_path is not None:
         stream = resource_stream("ututi", default_img_path).read()
-        return prepare_image(stream, width, height)
+        return prepare_image(stream, width, height, square)
     else:
         return None
 
 
-def prepare_image(image, width=None, height=None):
+def prepare_image(image, width=None, height=None, square=False):
     img = Image.open(StringIO.StringIO(image))
-    if width is not None or height is not None:
+    if square:
+        img = crop_square(img, width)
+    elif width is not None or height is not None:
         img = resize_image(img, width=width, height=height)
     buffer = StringIO.StringIO()
     img.save(buffer, "PNG")
@@ -83,18 +85,44 @@ def resize_image(image, width=300, height=300):
     width = width or 300
     height = height or 300
 
-    width = min(300, int(width))
-    height = min(300, int(height))
+    max_width = min(300, int(width))
+    max_height = min(300, int(height))
 
-    width = float(width)
-    height = float(height)
-    limit_x = width / height
+    width, height = image.size # actual size
 
-    original_x = float(image.size[0]) / image.size[1]
+    ratio = float(max_width) / width
+    width, height = width * ratio, height * ratio
 
-    if limit_x > original_x:
-        width = height * original_x
-    elif limit_x <= original_x:
-        height = width / original_x
+    if height > max_height:
+        ratio = float(max_height) / height
+        width, height = width * ratio, height * ratio
 
     return image.resize((int(width), int(height)), PIL.Image.ANTIALIAS)
+
+def crop_square(image, size=None):
+    """Crop PIL image to square of specified size by
+    removing some width or height.
+
+    The result image is centered.
+
+    Size is capped at 300.
+    """
+    width, height = image.size
+
+    if size is None:
+        size = min(width, height)
+
+    size = min(300, int(size))
+
+    if width > height:
+        ratio = float(size) / height
+        width, height = int(width * ratio), int(height * ratio)
+        x = (width - height) / 2
+        box = (x, 0, x + height, height)
+    else:
+        ratio = float(size) / width
+        width, height = int(width * ratio), int(height * ratio)
+        y = (height - width) / 2
+        box = (0, y, width, y + width)
+
+    return image.resize((width, height), PIL.Image.ANTIALIAS).crop(box)
