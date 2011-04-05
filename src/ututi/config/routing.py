054a4a094a4c92d7b8e36a3ad71d09e19473d9d0
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-04-05 13:00:19 +0300

    Removed one more deprecated route.

--- src/ututi/config/routing.py
+++ src/ututi/config/routing.py
@@ -292,7 +292,6 @@ def make_map(config):
     map.connect('/login', controller='home', action='login')
     map.connect('/logout', controller='home', action='logout')
     map.connect('/register', controller='home', action='register')
-    map.connect('/federated_registration', controller='home', action='federated_registration')
     map.connect('/google_login', controller='federation', action='google_login')
     map.connect('/google_verify', controller='federation', action='google_verify')
     map.connect('/facebook_login', controller='federation', action='facebook_login')
