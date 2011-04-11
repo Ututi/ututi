a9915c802e141fed4e857ba0bea7c720b3e5e10a
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-04-11 15:18:13 +0300

    Fixed data.sql to specify member_policy for department.

--- src/ututi/model/data.sql
+++ src/ututi/model/data.sql
@@ -1,7 +1,7 @@
 insert into tags (title, title_short, description, tag_type, member_policy)
        values ('U-niversity', 'uni', '', 'location', 'PUBLIC');
-insert into tags (title, title_short, description, parent_id, tag_type)
-       values ('D-epartment', 'd', '', 1, 'location');
+insert into tags (title, title_short, description, parent_id, tag_type, member_policy)
+       values ('D-epartment', 'd', '', 1, 'location', 'PUBLIC');
 
 insert into authors (type, fullname) values ('user', 'Administrator of the university');
 insert into users (id, location_id, username, password) values (1, 1, 'admin@uni.ututi.com', 'xnIVufqLhFFcgX+XjkkwGbrY6kBBk0vvwjA7');
