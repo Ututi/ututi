c5dddd6997e1af5bf041143f4897f517f34e4e3d
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-04-13 18:39:59 +0300

    Removed dead javascript.

--- src/ututi/public/javascript/wall.js
+++ src/ututi/public/javascript/wall.js
@@ -98,38 +98,4 @@ $(document).ready(function(){
         _gaq.push(['_trackEvent', category, action, label]);
     }
 
-    /*
-    $('.action_submit').click(function(evt) {
-        evt.stopPropagation();
-        var form = $(this).closest('form');
-        var event_snippet = $(this).closest('.wall_item');
-        if ($(event_snippet).hasClass('sms_sent')) {
-            var text = $("textarea[name='sms_message']", form).val();
-        } else {
-            var text = $("textarea[name='message']", form).val();
-        }
-
-        if ($.trim(text) != '') {
-            form.closest('.wall_item').addClass('loading');
-            $.post(form.attr('action')+'?js=1',
-              form.serialize(),
-              function(data, status) {
-                  return function() {
-                      if (status == 'success') {
-                          $(form).closest('.wall_item').removeClass('loading');
-                          $(form).closest('.action').fadeOut().replaceWith($('<div class="action-reply">'+data+'</div>'));
-                      }
-                  }(data, status, event_snippet);
-              }
-
-            );
-            return false;
-
-            action = form.attr('action');
-            data = form.serialize();
-        }
-        return false;
-    });
-
-    */
 });
