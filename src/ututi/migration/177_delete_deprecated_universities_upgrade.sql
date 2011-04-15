261c51be6ab5888b242a77b8dbe2c708e8ac1e37
Jaro Šatkevič <jaro@akl.lt>
2011-04-15 19:06:08 +0300

    Updated migration script.
    Not KMU but LSMU.
    Also we decided that VIKO and LZUU will be deleted.

--- src/ututi/migration/177_delete_deprecated_universities_upgrade.sql
+++ src/ututi/migration/177_delete_deprecated_universities_upgrade.sql
@@ -1,1 +1,1 @@
-DELETE FROM tags WHERE parent_id is null AND title_short NOT IN ('KMU','VU','UNI','VGTU','KTU','VPU','VIKO','LŽŪU');
+DELETE FROM tags WHERE parent_id is null AND title_short NOT IN ('LSMU','VU','UNI','VGTU','KTU','VPU');
