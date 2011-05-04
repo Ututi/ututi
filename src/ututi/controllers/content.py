2beb080e71b01a98b82f9353637c56580cd5933e
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-05-04 14:23:19 +0300

    Guard content controller actions with 404.

--- src/ututi/controllers/content.py
+++ src/ututi/controllers/content.py
@@ -1,6 +1,6 @@
 import logging
 
-from pylons.controllers.util import redirect
+from pylons.controllers.util import redirect, abort
 
 from ututi.lib.base import BaseController
 from ututi.model import ContentItem
@@ -11,13 +11,31 @@ log = logging.getLogger(__name__)
 
 class ContentController(BaseController):
 
-    def get_content(self, id, next_action=None):
+    def get_content(self, id=None, next_action=None):
+        try:
+            id = int(id)
+        except ValueError, TypeError:
+            abort(404)
+
         content_item = ContentItem.get(id)
+
+        if content_item is None:
+            abort(404)
+
         if next_action is None:
             redirect(content_item.url())
         else:
             redirect(content_item.url(action=next_action))
 
-    def get_user(self, id):
+    def get_user(self, id=None):
+        try:
+            id = int(id)
+        except ValueError, TypeError:
+            abort(404)
+
         user = User.get_byid(id)
+
+        if user is None:
+            abort(404)
+
         redirect(user.url())
