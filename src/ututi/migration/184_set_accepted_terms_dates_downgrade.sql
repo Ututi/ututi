1da5dfb67a57b18ead3aeded5f4222843ef26bb2
Linas Petrauskas <linas.petrauskas@gmail.com>
2011-05-19 16:50:56 +0300

    Added migration to fix accepted terms dates for old users.

---
+++ src/ututi/migration/184_set_accepted_terms_dates_downgrade.sql
@@ -0,0 +1,1 @@
+update users set accepted_terms = null where accepted_terms = '2009-01-01 12:00:00';
---
+++ src/ututi/migration/184_set_accepted_terms_dates_upgrade.sql
@@ -0,0 +1,1 @@
+update users set accepted_terms = '2009-01-01 12:00:00' where accepted_terms is null;
