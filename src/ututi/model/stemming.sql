22a65c7f412ce16e2d654f1b0b0d9c2c609d877f
Gytis Karčiauskas <gytis@karciauskas.lt>
2012-02-17 17:55:08 +0200

    Added new text search configuration.

--- src/ututi/model/stemming.sql
+++ src/ututi/model/stemming.sql
@@ -29,3 +29,9 @@ ALTER TEXT SEARCH CONFIGURATION pl
     ALTER MAPPING FOR asciiword, asciihword, hword_asciipart,
                       word, hword, hword_part
     WITH polish, english, simple;;
+
+CREATE TEXT SEARCH CONFIGURATION public.universal ( COPY = pg_catalog.english );;
+ALTER TEXT SEARCH CONFIGURATION universal
+    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart,
+                      word, hword, hword_part
+    WITH lithuanian, polish, english, simple;;
