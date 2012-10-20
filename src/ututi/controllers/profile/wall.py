d50f7277e4610ce31c23c3a14022f52af06193b8
Saulius Žemaitaitis <saulius@zemaitaitis.lt>
2012-10-20 18:57:21 +0300

    Unwrap commented event ids.

--- src/ututi/controllers/profile/wall.py
+++ src/ututi/controllers/profile/wall.py
@@ -28,21 +28,22 @@ class UserWallMixin(WallMixin):
         t_content_items = meta.metadata.tables['content_items']
         subject_ids = [s.id for s in subjects]
         group_ids = [m.group.id for m in c.user.memberships]
-        user_commented_evts = select([t_evt_comments.c.event_id],
-                                     from_obj=[t_evt_comments.join(t_content_items,
-                                                                   t_content_items.c.id == t_evt_comments.c.id)],)\
+        user_commented_evts_select = select([t_evt_comments.c.event_id],
+                                            from_obj=[t_evt_comments.join(t_content_items,
+                                                                          t_content_items.c.id == t_evt_comments.c.id)],)\
             .where(t_content_items.c.created_by == c.user.id)
+        user_commented_evts = map(lambda r: r[0], meta.Session.execute(user_commented_evts_select).fetchall())
 
         query = evts_generic\
-             .where(or_(or_(t_evt.c.object_id.in_(subject_ids),
-                            t_wall_posts.c.subject_id.in_(subject_ids)) if subject_ids else False,  # subject wall posts
-                        and_(or_(t_evt.c.author_id == c.user.id,  # location wall posts
-                                 # XXX User comments may grow to 1k-10k scale, consider a different implementation.
-                                 t_evt.c.id.in_(user_commented_evts) if user_commented_evts else False),
-                             t_evt.c.event_type.in_(('subject_wall_post', 'location_wall_post'))),
-                        or_(t_evt.c.object_id.in_(group_ids),) if group_ids else False))\
-             .where(or_(t_evt.c.event_type != 'moderated_post_created',
-                        t_evt.c.object_id.in_(user_is_admin_of_groups) if user_is_admin_of_groups else False))\
-             .where(not_(t_evt.c.event_type.in_(c.user.ignored_events_list) if c.user.ignored_events_list else False))
+            .where(or_(or_(t_evt.c.object_id.in_(subject_ids),
+                           t_wall_posts.c.subject_id.in_(subject_ids)) if subject_ids else False,  # subject wall posts
+                       and_(or_(t_evt.c.author_id == c.user.id,  # location wall posts
+                                # XXX User comments may grow to 1k-10k scale, consider a different implementation.
+                                t_evt.c.id.in_(user_commented_evts) if user_commented_evts else False),
+                            t_evt.c.event_type.in_(('subject_wall_post', 'location_wall_post'))),
+                       or_(t_evt.c.object_id.in_(group_ids),) if group_ids else False))\
+            .where(or_(t_evt.c.event_type != 'moderated_post_created',
+                       t_evt.c.object_id.in_(user_is_admin_of_groups) if user_is_admin_of_groups else False))\
+            .where(not_(t_evt.c.event_type.in_(c.user.ignored_events_list) if c.user.ignored_events_list else False))
 
         return query
