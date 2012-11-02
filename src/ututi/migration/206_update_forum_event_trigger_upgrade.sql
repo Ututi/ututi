4569fc475179698325bea9371cb0ca37d47b0447
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2012-11-02 15:48:28 +0200

    Updated forum event triggers so they would not ignore categories 1 and 2 anymore.

---
+++ src/ututi/migration/206_update_forum_event_trigger_upgrade.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION group_forum_message_event_trigger() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+    DECLARE
+      evt events;
+      pid int8 := NULL;
+    BEGIN
+      pid := get_group_forum_post_event_parent(NEW);
+      INSERT INTO events (object_id, author_id, event_type, post_id)
+             VALUES (
+                (SELECT group_id FROM forum_categories
+                 WHERE forum_categories.id = NEW.category_id),
+                cast(current_setting('ututi.active_user') as int8),
+                'forum_post_created', NEW.id)
+             RETURNING * INTO evt;
+      UPDATE events SET parent_id = evt.id WHERE parent_id = pid OR id = pid;
+      RETURN NEW;
+    END
+$$;
--- src/ututi/migration/__init__.py
+++ src/ututi/migration/__init__.py
@@ -5,7 +5,7 @@ import pkg_resources
 
 from martian.scan import module_info_from_dotted_name
 
-MIN_VERSION = 205
+MIN_VERSION = 206
 
 
 def sql_migrate(name):
