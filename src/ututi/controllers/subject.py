502ed562919d371edfef576d9c5e2d9377bf2bd8
Saulius Žemaitaitis <saulius@zemaitaitis.lt>
2012-11-05 10:28:12 +0200

    Subject permission page Lithuanian translations.

--- src/ututi/controllers/subject.py
+++ src/ututi/controllers/subject.py
@@ -107,7 +107,7 @@ def subject_privacy(method):
                 location_link = ((c.subject.location.url(), ' '.join(c.subject.location.full_title_path))
                                  if c.subject.visibility == 'department_members'
                                  else (c.subject.location.root.url(), c.subject.location.root.title))
-                request.environ['ututi.access_denied_reason'] = h.literal(('Only %(location)s members can access see this subject.')
+                request.environ['ututi.access_denied_reason'] = h.literal(_('Only %(location)s members can access see this subject.')
                     % dict(location=h.link_to(location_link[1], location_link[0])))
                 abort(403)
         c.user_can_edit_settings = c.user and (c.subject.edit_settings_perm == 'everyone' or check_crowds(['teacher', 'moderator'], c.user))
